76ed4c2f6d5a44198994e37fc54623faac0bfe28
JihwanB <leyqorwlghks@gmail.com>
2024-03-14 17:41:41 +0900

    Initial Commit

---
+++ src/main/java/com/soez/ezcheck/EzCheckApplication.java
@@ -0,0 +1,13 @@
+package com.soez.ezcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soez/ezcheck/EzCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soez.ezcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
