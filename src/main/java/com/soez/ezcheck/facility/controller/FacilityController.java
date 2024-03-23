8add322d47c77cd4af95a1ee9e0813dae869fc59
JihwanB <leyqorwlghks@gmail.com>
2024-03-23 15:46:49 +0900

    Added doc comments

--- src/main/java/com/soez/ezcheck/facility/controller/FacilityController.java
+++ src/main/java/com/soez/ezcheck/facility/controller/FacilityController.java
@@ -1,53 +1,80 @@
 package com.soez.ezcheck.facility.controller;
 
-import com.soez.ezcheck.entity.Facility;
-import com.soez.ezcheck.facility.domain.FacilityReservationDetailsDTO;
-import com.soez.ezcheck.facility.domain.FacilityReservationRequestDTO;
-import com.soez.ezcheck.facility.service.FacilityServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.sql.Date;
 import java.sql.Time;
 import java.util.Collections;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soez.ezcheck.entity.Facility;
+import com.soez.ezcheck.facility.domain.FacilityReservationDetailsDTO;
+import com.soez.ezcheck.facility.domain.FacilityReservationRequestDTO;
+import com.soez.ezcheck.facility.service.FacilityServiceImpl;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/facility")
 @RequiredArgsConstructor
 public class FacilityController {
 
-    private final FacilityServiceImpl facilityService;
+	private final FacilityServiceImpl facilityService;
 
-    @GetMapping("/list")
-    public ResponseEntity<List<Facility>> listFacilities(@RequestBody FacilityReservationRequestDTO requestDTO) {
-        Integer peopleToReserve = requestDTO.getPeopleToReserve();
-        Date date = requestDTO.getDate();
-        Time time = requestDTO.getTime();
+	/**
+	 * 예약하려는 인원수, 날짜, 시간을 고려해서 예약가능한 시설물을 조회하는 기능
+	 * @author Jihwan
+	 * @param requestDTO 예약을 원하는 인원수, 날짜, 시간
+	 * @return 조회된 예약이 가능한 시설물들 리스트 List<>
+	 */
+	@GetMapping("/list")
+	public ResponseEntity<List<Facility>> listFacilities(@RequestBody FacilityReservationRequestDTO requestDTO) {
+		Integer peopleToReserve = requestDTO.getPeopleToReserve();
+		Date date = requestDTO.getDate();
+		Time time = requestDTO.getTime();
 
-        List<Facility> availableFacilities = facilityService.findAvailableFacilities(peopleToReserve);
+		// 예약하려는 인원수를 수용가능한 시설물 조회
+		List<Facility> availableFacilities = facilityService.findAvailableFacilities(peopleToReserve);
 
-        if (availableFacilities.isEmpty()) {
-            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
-        }
+		if (availableFacilities.isEmpty()) {
+			return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+		}
 
-        // Make reservations for reservable facilities
-        List<Facility> reservableFacilities = facilityService.findReservableFacilities(availableFacilities, peopleToReserve, date, time);
+		// 인원수에 더해서 날짜와 시간까지 고려했을 때에도 예약이 가능한 시설물들 조회
+		List<Facility> reservableFacilities = facilityService.findReservableFacilities(availableFacilities,
+			peopleToReserve, date, time);
+		// 조회된 예약이 가능한 시설물 목록을 사용자에게 반환
+		return new ResponseEntity<>(reservableFacilities, HttpStatus.OK);
+	}
 
-        return new ResponseEntity<>(reservableFacilities, HttpStatus.OK);
-    }
+	/**
+	 * 시설물을 특정해서 예약 레코드를 생성하는 메서드
+	 * @author Jihwan
+	 * @param requestDTO 시설물 ID, 사용자 ID, 예약할 날짜, 예약할 시간, 예약할 인원수
+	 * @return 날짜와 시간을 포함한 예약 완료 메시지
+	 */
+	@PostMapping("/reserve")
+	public ResponseEntity<String> makeReservation(@RequestBody FacilityReservationRequestDTO requestDTO) {
+		String msg = facilityService.makeReservation(requestDTO);
+		return new ResponseEntity<>(msg, HttpStatus.OK);
+	}
 
-    @PostMapping("/reserve")
-    public ResponseEntity<String> makeReservation(@RequestBody FacilityReservationRequestDTO requestDTO) {
-        String msg = facilityService.makeReservation(requestDTO);
-        return new ResponseEntity<>(msg, HttpStatus.OK);
-    }
-
-    @GetMapping("/user/{uId}")
-    public List<FacilityReservationDetailsDTO> getReservationDetails(@PathVariable("uId") String uId) {
-        return facilityService.getReservationDetails(uId);
-    }
+	/**
+	 * 사용자 ID로 생성되어 있는 예약 내역들을 조회
+	 * @author Jihwan
+	 * @param uId 사용자 ID
+	 * @return 예약내역(시설명, 예약 날짜, 예약 시간, 예약 인원수)들을 담은 List<>
+	 */
+	@GetMapping("/user/{uId}")
+	public List<FacilityReservationDetailsDTO> getReservationDetails(@PathVariable("uId") String uId) {
+		return facilityService.getReservationDetails(uId);
+	}
 
 }
