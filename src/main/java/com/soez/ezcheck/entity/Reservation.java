137728095cf971dbaa57a7681d8484dc6e3a2355
JihwanB <leyqorwlghks@gmail.com>
2024-03-25 14:17:48 +0900

    Altered cardinality of foreign key rg_id from OneToOne to ManyToOne

--- src/main/java/com/soez/ezcheck/entity/Reservation.java
+++ src/main/java/com/soez/ezcheck/entity/Reservation.java
@@ -1,32 +1,39 @@
 package com.soez.ezcheck.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "reservation")
 public class Reservation {
 
-    @Id
-    @Column(name = "rv_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer rvId;
+	@Id
+	@Column(name = "rv_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer rvId;
 
-    @Column(name = "rv_date_from")
-    private Date rvDateFrom;
+	@Column(name = "rv_date_from")
+	private Date rvDateFrom;
 
-    @Column(name = "rv_date_to")
-    private Date rvDateTo;
+	@Column(name = "rv_date_to")
+	private Date rvDateTo;
 
-    @ManyToOne
-    @JoinColumn(name = "u_id")
-    private Users users;
+	@ManyToOne
+	@JoinColumn(name = "u_id")
+	private Users users;
 
-    @OneToOne
-    @JoinColumn(name = "rg_id")
-    private RoomGrade roomGrade;
+	@ManyToOne
+	@JoinColumn(name = "rg_id")
+	private RoomGrade roomGrade;
 
 }
