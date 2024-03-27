7450d00433163e39b29e3b8ca83d42a499c6e6c1
chankyuu <shin4438@gmail.com>
2024-03-27 12:29:20 +0900

    start swagger

---
+++ src/main/java/com/soez/ezcheck/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.soez.ezcheck.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("EZCHECK API") // API의 제목
+                .description("EZCHECK Swagger UI") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
