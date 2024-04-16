4fc05afbf37b210c124674a156749dd52f73c974
JihwanB <leyqorwlghks@gmail.com>
2024-04-16 15:49:27 +0900

    Removed configs related to CORS

--- src/main/java/com/soez/ezcheck/config/CorsConfig.java
+++ src/main/java/com/soez/ezcheck/config/CorsConfig.java
@@ -1,17 +1,17 @@
-package com.soez.ezcheck.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8081")
-			.allowedMethods("GET", "POST", "PUT", "DELETE")
-			.allowedHeaders("*");
-	}
-}
+// package com.soez.ezcheck.config;
+//
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+//
+// 	@Override
+// 	public void addCorsMappings(CorsRegistry registry) {
+// 		registry.addMapping("/**")
+// 			.allowedOrigins("http://localhost:8081")
+// 			.allowedMethods("GET", "POST", "PUT", "DELETE")
+// 			.allowedHeaders("*");
+// 	}
+// }
